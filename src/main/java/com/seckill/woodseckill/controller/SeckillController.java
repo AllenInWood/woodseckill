36986aee34ca9d56729bea06527182669b97313b
alleninwood <alleninwood@gmail.com>
2019-04-23 01:20:59 -0700

    make do_seckill static

--- src/main/java/com/seckill/woodseckill/controller/SeckillController.java
+++ src/main/java/com/seckill/woodseckill/controller/SeckillController.java
@@ -5,6 +5,7 @@ import com.seckill.woodseckill.domain.SeckillOrder;
 import com.seckill.woodseckill.domain.SeckillUser;
 import com.seckill.woodseckill.redis.RedisService;
 import com.seckill.woodseckill.result.CodeMsg;
+import com.seckill.woodseckill.result.Result;
 import com.seckill.woodseckill.service.GoodsService;
 import com.seckill.woodseckill.service.OrderService;
 import com.seckill.woodseckill.service.SeckillService;
@@ -13,9 +14,7 @@ import com.seckill.woodseckill.vo.GoodsVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,31 +37,28 @@ public class SeckillController {
     @Autowired
     SeckillService seckillService;
 
-    @RequestMapping("/do_seckill")
-    public String doSeckill(Model model,
+    @RequestMapping(value = "/do_seckill", method = RequestMethod.POST)
+    @ResponseBody
+    public Result<OrderInfo> seckill(Model model,
                             SeckillUser user,
                             @RequestParam("goodsId") long goodsId) {
         model.addAttribute("user", user);
         if (user == null) {
-            return "login";
+            return Result.error(CodeMsg.SESSION_ERROR);
         }
         // if seckill stock > 0
         GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
         int stock = goods.getStockCount();
         if (stock <= 0) {
-            model.addAttribute("err_msg", CodeMsg.SECOND_KILL_OVER.getMsg());
-            return "seckill_fail";
+            return Result.error(CodeMsg.SECOND_KILL_OVER);
         }
         // if success kill exists (in case duplicate kill)
         SeckillOrder seckillOrder = orderService.getSeckillOrderByUserIdGoodsId(user.getId(), goodsId);
         if (seckillOrder != null) {
-            model.addAttribute("err_msg", CodeMsg.REPEAT_SECOND_KILL.getMsg());
-            return "seckill_fail";
+            return Result.error(CodeMsg.REPEAT_SECOND_KILL);
         }
         // can second kill: 1. minus stock 2. create order 3. write into seckill order
         OrderInfo orderInfo = seckillService.seckill(user, goods);
-        model.addAttribute("orderInfo", orderInfo);
-        model.addAttribute("goods", goods);
-        return "order_detail";
+        return Result.success(orderInfo);
     }
 }
