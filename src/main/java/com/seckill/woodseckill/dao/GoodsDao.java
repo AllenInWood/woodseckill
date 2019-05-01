65f869c9beb4ad2407aca55774dbc3b344cc0d3c
alleninwood <alleninwood@gmail.com>
2019-05-01 10:52:34 -0700

    remove unused public

--- src/main/java/com/seckill/woodseckill/dao/GoodsDao.java
+++ src/main/java/com/seckill/woodseckill/dao/GoodsDao.java
@@ -14,7 +14,7 @@ import java.util.List;
 public interface GoodsDao {
 
     @Select("select g.*, sg.seckill_price, sg.stock_count, sg.start_date, sg.end_date from seckill_goods sg left join goods g on sg.goods_id = g.id")
-    public List<GoodsVo> listGoodsVo();
+    List<GoodsVo> listGoodsVo();
 
     @Select("select g.*, sg.seckill_price, sg.stock_count, sg.start_date, sg.end_date from seckill_goods sg left join goods g on sg.goods_id = g.id where g.id = #{goodsId}")
     GoodsVo getGoodsVoByGoodsId(@Param("goodsId") long goodsId);
